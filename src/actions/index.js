42db5b8280d9e70bdd55b1e5ef72db430e51ae1e
Paul WB <higgsbison@gmail.com>
2016-12-28 15:49:43 -0800

    add searchbar functionality with axios and redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '981f466cb547b7dcfd7d39338579c4d5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},CA`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  }
+}
--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
 
-    this.onInputChange = this.onInputChange.bind(this)
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   onInputChange(e) {
     this.setState({term: e.target.value})
   }
   onFormSubmit(e) {
     e.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
   render() {
     return (
@@ -31,3 +37,8 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
